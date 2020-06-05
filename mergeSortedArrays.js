e2320a40b35e6528c61bc756145802bc8b42155a
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-05 09:30:28 +0100

    Add mergeSortedArrays

---
+++ mergeSortedArrays.js
@@ -0,0 +1,23 @@
+function mergeSortedArrays(left, right) {
+  let i = 0;
+  let j = 0;
+  let mergedArray = [];
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      mergedArray.push(left[i++]);
+    } else {
+      mergedArray.push(right[j++]);
+    }
+  }
+
+  while (i < left.length) {
+    mergedArray.push(left[i++]);
+  }
+
+  while (j < right.length) {
+    mergedArray.push(right[j++]);
+  }
+
+  return mergedArray;
+}
