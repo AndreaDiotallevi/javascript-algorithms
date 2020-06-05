e7a14f8f2cd3632df0356e5dec50a4695f629110
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-05 11:47:35 +0100

    Add quickSort

---
+++ quickSort.js
@@ -0,0 +1,17 @@
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+}
+
+module.exports = quickSort;
